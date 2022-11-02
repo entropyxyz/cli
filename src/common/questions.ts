250685b7c9cee3bd5cad8ca2ff99d6fa72c9c9cb
JesseAbram <jesseabramowitz@hotmail.com>
2022-11-02 19:08:22 -0400

    add in endpoint gets

--- src/common/questions.ts
+++ src/common/questions.ts
@@ -1,6 +1,6 @@
 import inquirer from "inquirer";
-import { getWallet } from "./entropy";
-const question = [
+
+const questionMnemonic = [
   {
     type: "input",
     name: "mnemonic",
@@ -9,12 +9,50 @@ const question = [
   },
 ];
 
+const questionThresholdEndpoints = [
+  {
+    type: "input",
+    name: "thresholdEndpoints",
+    message: "input threshold endpoints",
+    default: '["http://127.0.0.1:3001", "http://127.0.0.1:3002"]',
+  },
+];
+
+const questionChainEndpoint = [
+  {
+    type: "input",
+    name: "chainEndpoints",
+    message: "input mnemonic",
+    default: 'ws://127.0.0.1:9944',
+  },
+];
+
 export const handleMnemonic = async () => {
   if (process.env.MNEMONIC) {
     return process.env.MNEMONIC;
   }
 
-  const { mnemonic } = await inquirer.prompt(question);
+  const { mnemonic } = await inquirer.prompt(questionMnemonic);
 
-  return await getWallet(mnemonic);
+  return mnemonic;
+};
+
+export const handleThresholdEndpoints = async () => {
+  if (process.env.ENDPOINTS_THRESHOLD) {
+    return JSON.parse(process.env.ENDPOINTS_THRESHOLD);
+  }
+
+  const { thresholdEndpoints } = await inquirer.prompt(
+    questionThresholdEndpoints
+  );
+  return JSON.parse(thresholdEndpoints);
+};
+
+export const handleChainEndpoint = async () => {
+  if (process.env.ENDPOINT_CHAIN) {
+    return process.env.ENDPOINT_CHAIN;
+  }
+
+  const { chainEndpoint } = await inquirer.prompt(questionChainEndpoint);
+  return chainEndpoint
 };
--- src/flows/register/index.ts
+++ src/flows/register/index.ts
@@ -1,6 +1,10 @@
-import { handleMnemonic } from "../../common/questions";
+import { handleMnemonic, handleThresholdEndpoints, handleChainEndpoint } from "../../common/questions";
+import { getWallet } from "../../common/entropy";
 
 export const register = async () => {
-  const wallet = await handleMnemonic();
-  console.log({ wallet });
+  const mnemonic = await handleMnemonic();
+  const wallet = await getWallet(mnemonic);
+  const thresholdEndpoints = await handleThresholdEndpoints()
+  const chainEndpoint = await handleChainEndpoint()
+  console.log({ wallet, thresholdEndpoints, chainEndpoint });
 };
