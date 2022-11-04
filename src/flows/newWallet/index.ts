64e8399f396b23fb01def95680fbbfa6ca8d3498
JesseAbram <jesseabramowitz@hotmail.com>
2022-11-04 15:00:53 -0400

    clean

--- src/flows/newWallet/index.ts
+++ src/flows/newWallet/index.ts
@@ -1,20 +1,13 @@
-import {
-	mnemonicGenerate,
-	mnemonicToMiniSecret,
-	mnemonicValidate,
-	ed25519PairFromSecret,
-	randomAsU8a,
-	randomAsHex
-  } from '@polkadot/util-crypto';
-  import {u8aToString} from "@polkadot/util"
-  import { Keyring } from "@polkadot/api";
+import { randomAsHex } from "@polkadot/util-crypto";
+import { Keyring } from "@polkadot/api";
 
 export const newWallet = async () => {
-	const seed: any = randomAsHex(32);
-	const keyring = new Keyring({ type: "sr25519" });
+  const seed: any = randomAsHex(32);
+  const keyring = new Keyring({ type: "sr25519" });
 
-	const wallet = keyring.addFromSeed(seed);
-	console.log("take the seed and add it to the .env", {wallet: wallet.address, seed})
-
-
-}
+  const wallet = keyring.addFromSeed(seed);
+  console.log("take the seed and add it to the .env", {
+    wallet: wallet.address,
+    seed,
+  });
+};
