c9babb013aa64074741fa51ba664b6b13ff6bafa
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2023-04-20 14:42:58 -0400

    give zaps (#4)

--- src/flows/index.ts
+++ src/flows/index.ts
@@ -2,3 +2,4 @@ export { register } from "./register";
 export { sign } from "./sign";
 export { entropyFaucet } from "./entropyFaucet";
 export { newWallet } from "./newWallet";
+export { giveZaps } from "./zaps";
--- src/flows/register/index.ts
+++ src/flows/register/index.ts
@@ -1,9 +1,6 @@
-import {
-  handleSeed,
-  handleKeyPath,
-} from "../../common/questions";
+import { handleSeed, handleKeyPath } from "../../common/questions";
 import { readKey } from "../../common/utils";
-import Entropy from "@entropyxyz/entropy-js"
+import Entropy from "@entropyxyz/entropy-js";
 
 export const register = async () => {
   const seed = await handleSeed();
@@ -11,13 +8,13 @@ export const register = async () => {
   const threshold_key = readKey(`tofn/${name}/0`);
   const threshold_key_bob = readKey(`tofn/${name}/1`);
   const entropy = await Entropy.setup(seed);
-  console.log(entropy.substrate.signer.wallet.address)
-  await entropy.substrate.api.isReady
-  const register = await entropy.register(
-    [threshold_key, threshold_key_bob],
-    entropy.substrate.signer.wallet.address,
-    false
-  );
-  console.log({register})
+  console.log(entropy.substrate.signer.wallet.address);
+  await entropy.substrate.api.isReady;
+  const register = await entropy.register({
+    keyShares: [threshold_key, threshold_key_bob],
+    constraintModificationAccount: entropy.substrate.signer.wallet.address,
+    freeTx: false,
+  });
+  console.log({ register });
   process.exit();
 };
