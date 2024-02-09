38255d4932955675775617ecb00d5e48479d61a1
frankie <frankie.diamond@gmail.com>
2024-02-09 08:16:38 -1000

    update balance flow

--- src/flows/balance/index.ts
+++ src/flows/balance/index.ts
@@ -1,54 +1,43 @@
 import inquirer from "inquirer"
 import { handleChainEndpoint, handleUserSeed } from "../../common/questions"
 import { Controller } from "../../../controller"
-import { returnToMain } from "../../common/utils"
+import { returnToMain, isValidSubstrateAddress } from "../../common/utils"
 import { initializeEntropy } from "../../common/initializeEntropy"
 
-const hexToBigInt = (hexString: string) => BigInt(hexString)
-
-export const balance = async (controller: Controller) => {
-  try {
-    const seed = await handleUserSeed()
-    const endpoint = await handleChainEndpoint()
-    const entropy = await initializeEntropy(seed, endpoint)
-
-    const balanceChoice = await inquirer.prompt([
-      {
-        type: "list",
-        name: "action",
-        message: "Choose an action:",
-        choices: ["Check my balance", "Query an address balance"],
-      },
-    ])
-
-    let accountToCheck
-    if (balanceChoice.action === "Check my balance") {
-      accountToCheck = entropy.account?.sigRequestKey?.wallet.address
-      if (!accountToCheck) {
-        throw new Error("User address not found")
-      }
-    } else {
-      const question = {
-        type: "input",
-        name: "account",
-        message: "Input account to check balance for:",
-        default: "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
-      }
-      const { account: inputAccount } = await inquirer.prompt([question])
-      accountToCheck = inputAccount
-    }
-
-    const accountInfo = (await entropy.substrate.query.system.account(accountToCheck)) as any
-    const freeBalance = hexToBigInt(accountInfo.data.free)
-    console.log(`Address ${accountToCheck} has free balance: ${freeBalance.toString()} bits`)
-  } catch (error: any) {
-    console.error("Error in balance:", error.message)
-  } finally {
-    if (await returnToMain()) {
-      console.clear()
-      controller.emit('returnToMain')
-    } else {
-      controller.emit('exit')
-    }
+export async function checkBalance ({accounts, endpoint}) {
+  const accountQuestion = {
+    type: "list",
+    name: "account",
+    message: "Choose account:",
+    choices: [ ...flattenAccountKeys(accounts), 'other']
   }
+
+  const otherQuestion = {
+    type: "input",
+    name: "account",
+    message: "Check balance of:",
+    when: ({ account }) => { return (account === 'other') }
+  }
+
+  const { account } = await inquirer.prompt([ accountQuestion, otherQuestion ])
+  const entropy = await initializeEntropy(null, endpoint)
+  const accountInfo = (await entropy.substrate.query.system.account(account)).toHuman()
+  // @ts-ignore: next line
+  console.log(`Address ${account} has free balance: ${accountInfo.balance.free} bits`)
+}
+function flattenAccountKeys (entropyAccounts) {
+  return entropyAccounts.reduce((agg, account) => {
+    if (account.sigRequestKey) {
+      const address = account.sigRequestKey.wallet.address
+      if (!agg.include(address)) agg.push(address)
+    }
+    if (account.programModKey) {
+      const address = account.programModKey.wallet.address
+      if (!agg.include(address)) agg.push(address)
+    }
+    if (account.programDeployKey) {
+      const address = account.programDeployKey.wallet.address
+      if (!agg.include(address)) agg.push(address)
+    }
+  }, {})
 }
