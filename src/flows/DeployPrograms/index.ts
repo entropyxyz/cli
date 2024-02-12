3e658d9640130b57bb99d7aa9a99b740f1f051ca
jawndiego <jawndiego@gmail.com>
2024-02-12 08:08:15 -0500

    deploy programs flow

--- src/flows/DeployPrograms/index.ts
+++ src/flows/DeployPrograms/index.ts
@@ -1,94 +1,113 @@
 import inquirer from "inquirer"
-import { handleUserSeed, handleChainEndpoint } from "../../common/questions"
-import { readFileSync } from "fs"
-import { getUserAddress } from "../../common/utils"
-import { Controller } from "../../../controller"
 import { initializeEntropy } from "../../common/initializeEntropy"
-import { returnToMain } from "../../common/utils"
-import * as util from '@polkadot/util'
+import { readFileSync } from "fs"
+import * as util from "@polkadot/util"
 
-export const devPrograms = async (controller: Controller) => {
-  try {
-    const seed = await handleUserSeed()
-    const endpoint = await handleChainEndpoint()
-    const userAddress = await getUserAddress()
-    const entropy = await initializeEntropy(seed, endpoint)
+export async function devPrograms ({ accounts, endpoint }) {
+  const accountQuestion = {
+    type: "list",
+    name: "selectedAccount",
+    message: "Choose account:",
+    choices: accountChoices(accounts),
+  }
+  const answers = await inquirer.prompt([accountQuestion])
+  const selectedAccount = answers.selectedAccount
+  let accountData
 
-    if (!entropy.account?.sigRequestKey) {
-      throw new Error("Keys are undefined")
-    }
+  accountData = selectedAccount.data
+  console.log("Selected account data:", accountData.seed)
 
-    const actionChoice = await inquirer.prompt([
+  const actionChoice = await inquirer.prompt ([
+    {
+      type: "list",
+      name: "action",
+      message: "Select your action:",
+      choices: ["Deploy", "Get Program Pointers", "Exit"],
+    },
+  ])
+
+  const entropy = await initializeEntropy (accountData.seed, endpoint)
+
+  switch (actionChoice.action) {
+    case "Deploy":
+      await deployProgram(entropy, selectedAccount)
+      break
+    case "Get Program Pointers":
+      await getProgramPointers(entropy, selectedAccount)
+      break
+    case "Exit":
+      console.log("Exiting.")
+      break
+  }
+}
+
+async function deployProgram (entropy, account) {
+  const deployQuestions = [
+    {
+      type: "input",
+      name: "programPath",
+      message: "Please provide the path to your program:",
+    },
+    {
+      type: "confirm",
+      name: "hasConfig",
+      message: "Does your program have a configuration file?",
+      default: false,
+    },
+  ]
+
+  const deployAnswers = await inquirer.prompt(deployQuestions)
+  const userProgram = readFileSync(deployAnswers.programPath)
+
+  let programConfig = ""
+
+  if (deployAnswers.hasConfig) {
+    const configAnswers = await inquirer.prompt([
       {
-        type: "list",
-        name: "action",
-        message: "Do you want to deploy a new program or get existing program pointers?",
-        choices: ["Deploy", "Get Program Pointers", "Exit to Main Menu"],
+        type: "input",
+        name: "config",
+        message: "Please provide your program configuration as a JSON string:",
       },
     ])
 
-    switch (actionChoice.action) {
-    case "Deploy": {
-      const answers = await inquirer.prompt([
-        {
-          type: "input",
-          name: "programPath",
-          message: "Please provide the path to your program file:",
-          validate: input => {
-            if (input) return true
-            else return "A valid path to a program file is required!"
-          },
-        },
-        {
-          type: "confirm",
-          name: "hasConfig",
-          message: "Does your program have a configuration?",
-          default: false,
-        },
-      ])
-
-      const userProgram: any = readFileSync(answers.programPath)
-      let programConfig = ''
-
-      if (answers.hasConfig) {
-        const configAnswers = await inquirer.prompt([
-          {
-            type: "input",
-            name: "config",
-            message: "Please provide your program configuration as a JSON string:",
-          },
-        ])
-
-        // Convert JSON string to bytes and then to hex
-        const encoder = new TextEncoder()
-        const byteArray = encoder.encode(configAnswers.config)
-        programConfig = util.u8aToHex(new Uint8Array(byteArray))
-      }
-
-      const pointer = await entropy.programs.dev.deploy(userProgram, programConfig)
-      console.log("Program deployed successfully with pointer:", pointer)
-      break
-    }
-    case "Get Program Pointers": {
-      console.log(userAddress)
-      if (!userAddress) return
-      const fetchedProgram = await entropy.programs.get(userAddress)
-      console.log('Retrieved program pointers:', fetchedProgram)
-      break
-    }
-
-    case "Exit to Main Menu": {
-      console.clear()
-      controller.emit('returnToMain')
-      break
-    }
-    }
-  } catch (error) {
-    console.error("Error in setProgram:", error)
-  } finally {
-    if (await returnToMain()) {
-      console.clear()
-      controller.emit('returnToMain')
-    }
+    // Convert JSON string to bytes and then to hex
+    const encoder = new TextEncoder()
+    const byteArray = encoder.encode(configAnswers.config)
+    programConfig = util.u8aToHex(new Uint8Array(byteArray))
   }
+
+  try {
+    // Deploy the program with config
+    const pointer = await entropy.programs.dev.deploy(
+      userProgram,
+      programConfig
+    )
+    console.log("Program deployed successfully with pointer:", pointer)
+  } catch (deployError) {
+    console.error("Deployment failed:", deployError)
+  }
+
+  console.log(`Deploying from account: ${account.address}`)
+}
+
+async function getProgramPointers (entropy, account) {
+  const userAddress = account.address
+  console.log(userAddress)
+  if (!userAddress) return
+
+  try {
+    const fetchedProgram = await entropy.programs.get(userAddress)
+    console.log("Retrieved program pointers:", fetchedProgram)
+  } catch (error) {
+    console.error("Failed to retrieve program pointers:", error)
+  }
+}
+
+function accountChoices (accounts) {
+  return accounts
+    .map((account) => ({
+      name: `${account.name} (${account.address})`,
+      value: account,
+    }))
+    .concat([{ name: "Other", value: null }])
 }
