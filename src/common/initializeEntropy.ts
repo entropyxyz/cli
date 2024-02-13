764e5407b5402b495a18340d3e0e9aad318d9022
jawndiego <jawndiego@gmail.com>
2024-02-13 12:58:50 -0500

    password retry

--- src/common/initializeEntropy.ts
+++ src/common/initializeEntropy.ts
@@ -6,31 +6,46 @@ import inquirer from "inquirer"
 
 export const initializeEntropy = async ({data}, endpoint: string): Promise<Entropy> => {
   if (Object.keys(data).length === 0) {
-    const entropy = new Entropy({ endpoint });
-    await entropy.ready;
-    return entropy;
+    const entropy = new Entropy({ endpoint })
+    await entropy.ready
+    return entropy
   }
 
-  let accountData;
+  let accountData
   if (data && typeof data === 'object' && 'type' in data && 'seed' in data) {
     accountData = data
   } else if (typeof data === 'string') {
-    const answers = await inquirer.prompt([
-      {
-        type: 'password',
-        name: 'password',
-        message: 'Enter password to decrypt data:',
-        mask: '*',
-      }
-    ])
-    const decryptedData = decrypt(data, answers.password)
 
-    //@ts-ignore
-    if (!decryptedData || typeof decryptedData !== 'object' || !('seed' in decryptedData)) {
-      throw new Error("Failed to decrypt data or decrypted data is invalid")
+    let decryptedData
+    let attempts = 0
+
+    while (attempts < 3) {
+      const answers = await inquirer.prompt([
+        {
+          type: 'password',
+          name: 'password',
+          message: 'Enter password to decrypt data:',
+          mask: '*',
+        }
+      ])
+
+      try {
+        decryptedData = decrypt(data, answers.password)
+        //@ts-ignore
+        if (!decryptedData || typeof decryptedData !== 'object' || !('seed' in decryptedData)) {
+          throw new Error("Failed to decrypt data or decrypted data is invalid")
+        }
+
+        break
+      } catch (error) {
+        console.error("Incorrect password. Try again")
+        attempts++
+        if (attempts >= 3) {
+          throw new Error("Failed to decrypt data after 3 attempts.")
+        }
+      }
     }
 
-    // Since we've thrown an error for the null or invalid case, we can assert non-null here
     accountData = decryptedData as { seed: string; type: string }
   } else {
     throw new Error("Data format is not recognized as either encrypted or unencrypted")
@@ -42,8 +57,7 @@ export const initializeEntropy = async ({data}, endpoint: string): Promise<Entro
     return entropy
   }
 
-  const { seed } = accountData
-  const signer = await getWallet(seed)
+  const signer = await getWallet(accountData.seed)
 
   const entropyAccount: EntropyAccount = {
     sigRequestKey: signer,
