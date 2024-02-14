fe0efdf719038ef1b9f1e69f21311f9ba46900a8
jawndiego <jawndiego@gmail.com>
2024-02-14 16:43:15 -0500

    specify programModKey in register

--- src/flows/register/index.ts
+++ src/flows/register/index.ts
@@ -13,49 +13,52 @@ export async function register ({ accounts, endpoints }, options) {
     choices: accountChoices(accounts),
   }
 
-  const answers = await inquirer.prompt([accountQuestion])
-  const selectedAccount = answers.selectedAccount
+  const selectedAccountAnswer = await inquirer.prompt([accountQuestion])
+  const selectedAccount = selectedAccountAnswer.selectedAccount
 
-  const entropy = await initializeEntropy(
-    { data: selectedAccount.data },
-    endpoint
-  )
+
+  const filteredAccountChoices = accountChoices(accounts).filter(choice => choice.name !== "Other")
+
+  const programModKeyAccountQuestion = {
+    type: "list",
+    name: "programModAccount",
+    message: "Choose account for programModKey or paste an address:",
+    choices: [
+      ...filteredAccountChoices,
+      new inquirer.Separator(),
+      { name: "Paste an address", value: "paste" },
+    ],
+  }
+
+
+  const programModAccountAnswer = await inquirer.prompt([programModKeyAccountQuestion])
+  let programModAccount
+
+  if (programModAccountAnswer.programModAccount === "paste") {
+    const pasteAddressQuestion = {
+      type: "input",
+      name: "pastedAddress",
+      message: "Paste the address here:",
+    }
+
+    const pastedAddressAnswer = await inquirer.prompt([pasteAddressQuestion])
+    programModAccount = pastedAddressAnswer.pastedAddress
+  } else {
+    programModAccount = programModAccountAnswer.programModAccount.address
+  }
+
+  const entropy = await initializeEntropy({ data: selectedAccount.data }, endpoint)
 
   await entropy.ready
 
-  const isRegistered = await entropy.registrationManager.checkRegistrationStatus(
-    selectedAccount.address
-  )
+  const isRegistered = await entropy.registrationManager.checkRegistrationStatus(selectedAccount.address)
 
   console.log("isRegistered", isRegistered)
 
   if (isRegistered) {
-    console.log("Address is already registered:", selectedAccount.address) 
+    console.log("Address is already registered:", selectedAccount.address)
   } else {
-
-    // allow list program
-    //   const pointer =
-    //     "0x7572505786b022118475733546a273d772b3857de537a0981c3e4a805678e3a0"
-
-    //   console.log("pointer", pointer)
-    //   const config = `
-    //     {
-    //         "allowlisted_addresses": [
-    //             "0x772b9a9e8aa1c9db861c6611a82d251db4fac990"
-    //         ]
-    //     }
-    // `
-    //   // convert to bytes
-
-    //   const encoder = new TextEncoder()
-    //   const byteArray = encoder.encode(config)
-
-    //   // convert u8a to hex
-    //   const programConfig = util.u8aToHex (new Uint8Array(byteArray))
-
-    // barebones program
-    const pointer =
-      "0x3873f6f91334cfb6cad84f94aa1e1025069405a4ea3577a818a5ad8d0e26bb39"
+    const pointer = "0x3873f6f91334cfb6cad84f94aa1e1025069405a4ea3577a818a5ad8d0e26bb39"
     const programConfig = "0x"
 
     const programData = {
@@ -67,16 +70,12 @@ export async function register ({ accounts, endpoints }, options) {
 
     console.log("Attempting to register the address:", selectedAccount.address)
     await entropy.register({
-      programModAccount: selectedAccount.address,
+      programModAccount: programModAccount,
       keyVisibility: "Permissioned",
       initialPrograms: [programData],
       freeTx: false,
     })
 
-    console.log(
-      "Your address",
-      selectedAccount.data.address,
-      "has been successfully registered."
-    )
+    console.log("Your address", selectedAccount.data.address, "has been successfully registered.")
   }
 }
