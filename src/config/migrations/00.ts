50937aa13f9186859a08f8f19a5e88472748d178
mixmix <mix@protozoa.nz>
2024-05-17 12:47:34 +1200

    linting hooks, tidy

--- src/config/migrations/00.ts
+++ src/config/migrations/00.ts
@@ -1,19 +1,19 @@
 export const version = '0'
 
 export function migrate (data= {}) {
-    try {
-      const migratedData = {
-        ...data,
-        accounts: [],
-        endpoints: {
-          dev: 'ws://127.0.0.1:9944',
-          'test-net': 'ws://testnet.entropy.xyz:9944/'
-        },
-        'migration-version': version,
-      }
-      return migratedData
-    } catch (e) {
-      console.error(`error in migration ${version}: e.message`)
+  try {
+    const migratedData = {
+      ...data,
+      accounts: [],
+      endpoints: {
+        dev: 'ws://127.0.0.1:9944',
+        'test-net': 'ws://testnet.entropy.xyz:9944/'
+      },
+      'migration-version': version,
     }
+    return migratedData
+  } catch (e) {
+    console.error(`error in migration ${version}: e.message`)
+  }
   return data
 }
