19b4492af97cdc0512e7709af3f800dab37f7835
Nayyir Jutha <nayyir@entropy.xyz>
2024-05-27 20:37:13 -0400

    wip: added action choices to sign flow with raw sign and sign with adapter

--- src/flows/sign/index.ts
+++ src/flows/sign/index.ts
@@ -1,53 +1,57 @@
-// import inquirer from "inquirer"
+import inquirer from "inquirer"
 import { initializeEntropy } from "../../common/initializeEntropy"
 import { debug, getSelectedAccount, print } from "../../common/utils"
 
 // TODO: revisit this file, rename as signEthTransaction?
 export async function sign ({ accounts, endpoints, selectedAccount: selectedAccountAddress }, options) {
   const endpoint = endpoints[options.ENDPOINT]
+  const actionChoice = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        "Raw Sign",
+        "Sign With Adapter",
+        "Exit to Main Menu",
+      ],
+    },
+  ])
 
-  // const accountQuestion = {
-  //   type: "list",
-  //   name: "selectedAccount",
-  //   message: "Choose account:",
-  //   choices: accountChoices(accounts),
-  // }
-
-  // const otherQuestion = {
-  //   type: "input",
-  //   name: "accountSeedOrPrivateKey",
-  //   message: "Enter the account seed or private key:",
-  //   when: (answers) => !answers.selectedAccount
-  // }
-
-  // const answers = await inquirer.prompt([accountQuestion, otherQuestion])
-  // const selectedAccount = answers.selectedAccount
   const selectedAccount = getSelectedAccount(accounts, selectedAccountAddress)
   debug("selectedAccount:", selectedAccount)
-  // const accountSeedOrPrivateKey = answers.accountSeedOrPrivateKey
   const keyMaterial = selectedAccount?.data;
-  // if (!keyMaterial || isEmpty(keyMaterial)) {
-  //   keyMaterial = {
-  //     seed: accountSeedOrPrivateKey,
-  //   }
-  // }
 
   const entropy = await initializeEntropy({ keyMaterial }, endpoint)
-
+  const msg = Buffer.from('Hello world: signature from entropy!').toString('hex')
+  debug('msg', msg);
   const { address } = entropy.keyring.accounts.registration
   debug("address:", address)
   if (address == undefined) {
     throw new Error("address issue")
   }
+  switch (actionChoice.action) {
+  case 'Raw Sign': {
+      
+    const signature = await entropy.sign({
+      sigRequestHash: msg,
+      hash: 'sha3',
+    })
 
-  const msg = Buffer.from('Hello world: signature from entropy!').toString('hex')
-  debug('msg', msg);
-  
+    print('signature:', signature)
+    return
+  }
+  case 'Sign With Adapter': {
+    const msgParam = { msg }
+    const signature =  await entropy.signWithAdaptersInOrder({
+      msg: msgParam,
+      order: ['deviceKeyProxy', 'noop'],
+    })
 
-  const signature = await entropy.sign({
-    sigRequestHash: msg,
-    hash: 'sha3',
-  })
-
-  print('signature:', signature)
+    print('signature:', signature)
+    return
+  }
+  default: 
+    throw new Error('Unrecognizable action')
+  }
 }
