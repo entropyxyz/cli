80c0773209a47723d4e20201888537e47149c6be
Nayyir Jutha <nayyir@entropy.xyz>
2024-06-21 16:41:06 -0400

    updated user flows to use the new pure function to grab balance

--- src/flows/balance/cli.ts
+++ src/flows/balance/cli.ts
@@ -1,8 +1,7 @@
 import { initializeEntropy } from '../../common/initializeEntropy'
 import * as config from '../../config'
 import { debug } from '../../common/utils'
-
-const hexToBigInt = (hexString: string) => BigInt(hexString)
+import { getBalance } from './balance'
 
 export async function cliGetBalance ({ address, password, endpoint }) {
   const storedConfig = await config.get()
@@ -17,10 +16,8 @@ export async function cliGetBalance ({ address, password, endpoint }) {
   }
 
   const entropy = await initializeEntropy({ keyMaterial: account.data, password, endpoint })
+  const balance = await getBalance(entropy, address)
 
-  const accountInfo = (await entropy.substrate.query.system.account(address)) as any
-  debug('accountInfo', accountInfo)
-
-  return hexToBigInt(accountInfo.data.free).toString()
+  return balance
 }
 
