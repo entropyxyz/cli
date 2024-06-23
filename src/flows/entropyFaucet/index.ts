53d1dbf096cac3f24aa269cac6289982ba726fa9
Nayyir Jutha <nayyir@entropy.xyz>
2024-06-23 17:53:43 -0400

    clean up code; removing commented code not needed anymore

--- src/flows/entropyFaucet/index.ts
+++ src/flows/entropyFaucet/index.ts
@@ -1,50 +1,40 @@
-import { blake2AsHex, encodeAddress, signatureVerify } from '@polkadot/util-crypto'
-import { debug, getSelectedAccount, stripHexPrefix } from "../../common/utils"
+import { blake2AsHex, encodeAddress } from '@polkadot/util-crypto'
+import { debug, getSelectedAccount } from "../../common/utils"
 import { initializeEntropy } from "../../common/initializeEntropy"
 import Entropy from "@entropyxyz/sdk"
 import FaucetSigner from "./signer"
-import { construct, decode, getRegistry, methods } from '@substrate/txwrapper-polkadot'
-import { toHex } from 'alchemy-sdk'
-import { u8aToHex } from '@polkadot/util'
 
 const faucetProgramModKey = '5GWamxgW4XWcwGsrUynqnFq2oNZPqNXQhMDfgNH9xNsg2Yj7'
 
 async function faucetSignAndSend (call: any, api: any, entropy: Entropy, amount: number, senderAddress: string, chosenVerifyingKey: any) {
   const faucetSigner = new FaucetSigner(api.registry, entropy, amount, chosenVerifyingKey)
-  const metadataHash = (await entropy.substrate.rpc.state.getMetadata()).hash
 
   const sig = await call.signAsync(senderAddress, {
     signer: faucetSigner,
   });
-  const blockHash = await entropy.substrate.rpc.chain.getBlockHash()
-  console.log({sig})
-  const test = await entropy.substrate.call.taggedTransactionQueue.validateTransaction("Local", sig, blockHash.toHex())
-  console.log({test: test.toHuman()})
-  // console.log({result})
-  // console.log({sig: sig.toHuman()})
+
   sig.send(({ status, dispatchError }: any) => {
     // status would still be set, but in the case of error we can shortcut
     // to just check it (so an error would indicate InBlock or Finalized)
     if (dispatchError) {
-        if (dispatchError.isModule) {
-            // for module errors, we have the section indexed, lookup
-            const decoded = api.registry.findMetaError(dispatchError.asModule);
-            const { documentation, method, section } = decoded;
+      if (dispatchError.isModule) {
+        // for module errors, we have the section indexed, lookup
+        const decoded = api.registry.findMetaError(dispatchError.asModule);
+        const { documentation, method, section } = decoded;
 
-            console.log(`${section}.${method}: ${documentation.join(' ')}`);
-            process.exit();
-        } else {
-            // Other, CannotLookup, BadOrigin, no extra info
-            console.log(dispatchError.toString());
-            process.exit();
-        }
-    } else {
-        if (status.isFinalized) {
-            console.log('\ntransaction successful');
-            process.exit();
-        }
+        console.log(`${section}.${method}: ${documentation.join(' ')}`)
+        process.exit()
+      } else {
+        // Other, CannotLookup, BadOrigin, no extra info
+        console.error(dispatchError.toString())
+        process.exit()
+      }
     }
-})
+    if (status.isFinalized) {
+      console.log('\ntransaction successful');
+      process.exit()
+    }
+  })
 }
 
 export async function entropyFaucet ({ accounts, selectedAccount: selectedAccountAddress }, options) {
@@ -64,117 +54,11 @@ export async function entropyFaucet ({ accounts, selectedAccount: selectedAccoun
   const verifyingKeys = modifiableKeys.toHuman()
   // Choosing one of the 5 verifiying keys at random to be used as the faucet sender
   const chosenVerifyingKey = verifyingKeys[Math.floor(Math.random() * (verifyingKeys as Array<string>).length)]
-  console.log('chosen key', chosenVerifyingKey);
   const hashedKey = blake2AsHex(chosenVerifyingKey)
-  console.log('hashed key', hashedKey);
   const faucetAddress = encodeAddress(hashedKey, 42).toString()
-  console.log('address', faucetAddress);
 
-  const { block } = await entropy.substrate.rpc.chain.getBlock()
-  const blockHash = await entropy.substrate.rpc.chain.getBlockHash()
-  const genesisHash = await entropy.substrate.rpc.chain.getBlockHash(0)
-  const metadataRpc = (await entropy.substrate.rpc.state.getMetadata())
-
-  const { specVersion, transactionVersion, specName } = await entropy.substrate.rpc.state.getRuntimeVersion()
-  const properties = await entropy.substrate.rpc.system.properties()
-
-  const registry = getRegistry({
-		chainName: 'Entropy',
-		specName: 'polkadot',
-		specVersion: specVersion.toNumber(),
-		metadataRpc: metadataRpc.toHex(),
-    properties: properties.toHuman(),
-	});
-
-  const unsigned = methods.balances.transferAllowDeath(
-		{
-			value: 10000000000000,
-			dest: { id: selectedAccountAddress }, // Bob
-		},
-		{
-			address: faucetAddress,
-			blockHash: blockHash.toHex(),
-			blockNumber: parseInt(registry.createType('BlockNumber', block.header.number).toHex()),
-			genesisHash: genesisHash.toString(),
-			metadataRpc: metadataRpc.toHex(),
-			nonce: 0, // Assuming this is Alice's first tx on the chain
-			specVersion: 100,
-			tip: 0,
-			transactionVersion: 6,
-		},
-		{
-			metadataRpc: metadataRpc.toHex(),
-			registry,
-		},
-	);
-  const transfer = await entropy.substrate.tx.balances.transferAllowDeath(selectedAccountAddress, amount);
+  const transfer = entropy.substrate.tx.balances.transferAllowDeath(selectedAccountAddress, amount);
   await faucetSignAndSend(transfer, entropy.substrate, entropy, parseInt(amount), faucetAddress, chosenVerifyingKey )
-  // const decodedUnsigned = decode(unsigned, {
-	// 	metadataRpc: metadataRpc.toHex(),
-	// 	registry,
-	// });
-	// console.log(
-	// 	`\nDecoded Transaction\n  To: ${
-	// 		(decodedUnsigned.method.args.dest as { id: string })?.id
-	// 	}\n` + `  Amount: ${decodedUnsigned.method.args.value}`,
-	// );
-  // const signingPayload = construct.signingPayload(unsigned, { registry });
-	// console.log(`\nPayload to Sign: ${signingPayload}`);
-
-	// // Decode the information from a signing payload.
-	// const payloadInfo = decode(signingPayload, {
-	// 	metadataRpc: metadataRpc.toHex(),
-	// 	registry,
-	// });
-	// console.log(
-	// 	`\nDecoded Transaction\n  To: ${
-	// 		(payloadInfo.method.args.dest as { id: string })?.id
-	// 	}\n` + `  Amount: ${payloadInfo.method.args.value}`,
-	// );
-  // const auxData = {
-  //   spec_version: 100,
-  //   transaction_version: 6,
-  //   string_account_id: entropy.keyring.accounts.registration.address,
-  //   amount: parseInt(amount)
-  // }
-
-  // const signature = await entropy.sign({
-  //   sigRequestHash: signingPayload,
-  //   hash: {custom: 0},
-  //   auxiliaryData: [auxData],
-  //   verifyingKeyOverwrite: chosenVerifyingKey
-  // })
-	// console.log(`\nSignature: ${signature}`);
-  // let sigHex = u8aToHex(signature);
-  // sigHex = `0x02${stripHexPrefix(sigHex)}`
-  // // Serialize a signed transaction.
-	// const tx = construct.signedTx(unsigned, sigHex, {
-	// 	metadataRpc: metadataRpc.toHex(),
-	// 	registry,
-	// });
-	// console.log(`\nTransaction to Submit: ${tx}`);
-
-  // // Derive the tx hash of a signed transaction offline.
-	// const expectedTxHash = construct.txHash(tx);
-	// console.log(`\nExpected Tx Hash: ${expectedTxHash}`);
-
-  // // Send the tx to the node. Again, since `txwrapper` is offline-only, this
-	// // operation should be handled externally. Here, we just send a JSONRPC
-	// // request directly to the node.
-  // const test = await entropy.substrate.call.taggedTransactionQueue.validateTransaction("local", tx, blockHash.toHex())
-  // console.log({test: test.toHuman()})
-	// const actualTxHash = await entropy.substrate.rpc.author.submitExtrinsic(tx)
-	// console.log(`Actual Tx Hash: ${actualTxHash}`);
-
-	// // Decode a signed payload.
-	// const txInfo = decode(tx, {
-	// 	metadataRpc: metadataRpc.toHex(),
-	// 	registry,
-	// });
-	// console.log(
-	// 	`\nDecoded Transaction\n  To: ${
-	// 		(txInfo.method.args.dest as { id: string })?.id
-	// 	}\n` + `  Amount: ${txInfo.method.args.value}\n`,
-	// );
+  
   return
 }
--- src/flows/entropyFaucet/signer.ts
+++ src/flows/entropyFaucet/signer.ts
@@ -1,10 +1,9 @@
 import Entropy from "@entropyxyz/sdk";
 import type { Signer, SignerResult } from "@polkadot/api/types";
-import { Registry, SignerPayloadJSON, SignerPayloadRaw } from "@polkadot/types/types";
-import { u8aConcat, u8aToHex, u8aWrapBytes } from "@polkadot/util";
-import { stripHexPrefix } from "../../common/utils";
+import { Registry, SignerPayloadJSON } from "@polkadot/types/types";
+import { u8aToHex } from "@polkadot/util";
+import { debug, stripHexPrefix } from "../../common/utils";
 import { blake2AsHex, decodeAddress, encodeAddress, signatureVerify } from "@polkadot/util-crypto";
-import { methods } from "@substrate/txwrapper-polkadot";
 
 let id = 0
 export default class FaucetSigner implements Signer {
@@ -44,6 +43,7 @@ export default class FaucetSigner implements Signer {
     console.log({auxData, vk: this.chosenVerifyingKey})
     const signature = await this.#entropy.sign({
       sigRequestHash: u8aToHex(raw),
+      // @ts-ignore
       hash: {custom: 0},
       auxiliaryData: [auxData],
       verifyingKeyOverwrite: this.chosenVerifyingKey
@@ -53,24 +53,16 @@ export default class FaucetSigner implements Signer {
     // the 02 prefix is needed for signature type edcsa (00 = ed25519, 01 = sr25519, 02 = ecdsa)
     // ref: https://github.com/polkadot-js/tools/issues/175#issuecomment-767496439
     sigHex = `0x02${stripHexPrefix(sigHex)}`
-    console.log({sigHex})
-    // const signature_test = "0x02c80347ab124efac91a73b60b17b306b8e24c902e5f3aaf66dc1077ddc7993b660517ab74a501cba14020cd7afb3dc988c2956ba888d07c83221b539188bc7d5a00"
-    // const sig = this.#registry.createType('EcdsaSignature', signature).toU8a(true)
-    // let submitable = this.#registry.createTypeUnsafe('ExtrinsicSignature', [sigHex]).toU8a(true)
-    // console.log({submitable, human: submitable.toHuman()})
-    const hashedKey = blake2AsHex(this.chosenVerifyingKey)
-    console.log('hashed key', hashedKey);
-    const faucetAddress = encodeAddress(hashedKey)
-    // signatureVerify()
 
+    const hashedKey = blake2AsHex(this.chosenVerifyingKey)
+    const faucetAddress = encodeAddress(hashedKey)
     const publicKey = decodeAddress(faucetAddress);
+
     const hexPublicKey = u8aToHex(publicKey);
-    console.log("test valid", signatureVerify(u8aToHex(raw), sigHex, hexPublicKey))
-    // console.log({signature: signature, realSig: u8aToHex(signature)})
+
+    const isSignatureValid = signatureVerify(u8aToHex(raw), sigHex, hexPublicKey)
+
+    debug({ isSignatureValid })
     return { id: id++, signature: sigHex };
   }
 }
-
-
-//0x55bd020bdbbdc02de34e915effc9b18a99002f4c29f64e22e8dcbb69e722ea6c28e1bb53b9484063fbbfd205e49dcc1f620929f520c9c4c3695150f05a28f52a01
-//0x58ec45b3f07577874aa0f45523b09aefdac1c25203235be473fa3ed412d081b540099ef34acc67509f3f23336c65dc5c03bfb09bdbc8b84b2bef2f25f4cdac9f00
