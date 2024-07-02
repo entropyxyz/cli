303811be7043a41cfd1576cc6cf027a318327b93
Nayyir Jutha <nayyir@entropy.xyz>
2024-07-02 17:30:26 -0400

    [NayNay] Debug Log
    - install and integrated winston logging library into cli
    - allowing us to be able to log any and everything to the respective files saved in the ~/path/to/syslogs/entropy-cryptography/entropy-cli.*.log
    - included example use of new logger in the tui flow for balance

--- src/flows/balance/index.ts
+++ src/flows/balance/index.ts
@@ -1,14 +1,17 @@
+import { EntropyLogger } from "src/common/logger";
 import { initializeEntropy } from "../../common/initializeEntropy"
-import { print, debug, getSelectedAccount } from "../../common/utils"
+import { print, getSelectedAccount } from "../../common/utils"
 import { getBalance } from "./balance";
 
 // TO-DO setup flow method to provide options to allow users to select account,
 // use external address, or get balances for all accounts in config
 export async function checkBalance ({ accounts, selectedAccount: selectedAccountAddress }, options) {
   const { endpoint } = options
-  debug('endpoint', endpoint);
+  const logger = new EntropyLogger('TUI:: checkBalance', endpoint)
+  logger.debug(`endpoint: ${endpoint}`)
   
   const selectedAccount = getSelectedAccount(accounts, selectedAccountAddress)
+  logger.log(`selected account: ${JSON.stringify(selectedAccount)}`)
   const entropy = await initializeEntropy({ keyMaterial: selectedAccount.data, endpoint });
   const accountAddress = selectedAccountAddress
   const freeBalance = await getBalance(entropy, accountAddress)
