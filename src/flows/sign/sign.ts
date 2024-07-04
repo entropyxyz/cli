13b8eec12e0a2b8a20161aeb1a5e9692c337872d
mixmix <mix@protozoa.nz>
2024-07-04 15:47:48 +1200

    patch sign reverse-order bug for CLI

--- src/flows/sign/sign.ts
+++ src/flows/sign/sign.ts
@@ -11,7 +11,7 @@ function stringToHex (str: string): string {
 }
 
 export async function signWithAdapters (entropy, input: SignWithAdapterInput) {
-  return entropy.signWithAdaptersInOrder({
+  const content = {
     msg: {
       msg: stringToHex(input.msg)
     },
@@ -19,5 +19,18 @@ export async function signWithAdapters (entropy, input: SignWithAdapterInput) {
     order: ['deviceKeyProxy', 'noop'],
     signatureVerifyingKey: input.verifyingKey
     // auxillaryData
-  })
+  }
+
+  return entropy.signWithAdaptersInOrder(content)
+    .catch(async (err) => {
+      // See https://github.com/entropyxyz/sdk/issues/367 for reasoning behind adding this retry mechanism
+      if (err.message.includes('Invalid Signer') || err.message.includes('Invalid Signer in Signing group')) {
+        // retry signing with a reverse order in the subgroups list
+        // QUESTION: will we need a different strategy to retry when there are >2 things to order
+        content.order = content.order.reverse()
+        return entropy.signWithAdaptersInOrder(content)
+      }
+
+      throw err
+    })
 }
