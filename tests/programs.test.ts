df36f05034012a9910c748fd3eca97ca5bc7b12a
mix irving <mix@protozoa.nz>
2024-08-01 13:59:23 +1200

    Mixmix/programs deploy (#206)

--- tests/programs.test.ts
+++ tests/programs.test.ts
@@ -1,57 +1,77 @@
 import test from 'tape'
-import { readFileSync } from 'node:fs'
 
 import { promiseRunner, charlieStashSeed, setupTest } from './testing-utils'
 import { addProgram } from '../src/flows/programs/add'
 import { viewPrograms } from '../src/flows/programs/view'
 import { removeProgram } from '../src/flows/programs/remove'
-import { AddProgramParams } from '../src/flows/programs/types'
+import { deployProgram } from '../src/flows/programs/deploy'
 
 const networkType = 'two-nodes'
 
 test('programs', async t => {
   const { run, entropy } = await setupTest(t, { seed: charlieStashSeed, networkType })
-  await run('charlie stash register', entropy.register())
-  const noopProgram: any = readFileSync(
-    new URL('./programs/program_noop.wasm', import.meta.url)
-  )
-  const newPointer = await run(
-    'deploy',
-    entropy.programs.dev.deploy(noopProgram)
-  )
+  await run('register', entropy.register()) // TODO: consider removing this in favour of just testing add
 
-  const noopProgramInstance: AddProgramParams = {
-    programPointer: newPointer,
-    programConfig: '',
-  }
+  let programPointer1
 
-  t.test('Add Program', async ap => {
-    const runAp = promiseRunner(ap)
+  t.test('programs - deploy', async t => {
+    const run = promiseRunner(t)
 
-    const programsBeforeAdd = await runAp('get programs initial', entropy.programs.get(entropy.programs.verifyingKey))
-    ap.equal(programsBeforeAdd.length, 1, 'charlie has 1 program')
-    await runAp('adding program', addProgram(entropy, noopProgramInstance))
-    const programsAfterAdd = await runAp('get programs after add', entropy.programs.get(entropy.programs.verifyingKey))
-    ap.equal(programsAfterAdd.length, 2, 'charlie has 2 programs')
-    ap.end()
+    programPointer1 = await run (
+      'deploy!',
+      deployProgram(entropy, {
+        bytecodePath: './tests/programs/program_noop.wasm'
+      })
+    )
+
+    t.end()
   })
 
-  t.test('Remove Program', async rp => {
-    const runRp = promiseRunner(rp)
-    const programsBeforeRemove = await runRp('get programs initial', entropy.programs.get(entropy.programs.verifyingKey))
-  
-    rp.equal(programsBeforeRemove.length, 2, 'charlie has 2 programs')
-    await runRp('removing noop program', removeProgram(entropy, { programPointer: newPointer, verifyingKey: entropy.programs.verifyingKey }))
-    const programsAfterRemove = await runRp('get programs initial', entropy.programs.get(entropy.programs.verifyingKey))
-    rp.equal(programsAfterRemove.length, 1, 'charlie has 1 less program')
-    rp.end()
+  const getPrograms = () => viewPrograms(entropy, { verifyingKey: entropy.programs.verifyingKey })
+  const verifyingKey = entropy.programs.verifyingKey
+
+  t.test('programs - add', async t => {
+    const run = promiseRunner(t)
+
+    const programsBeforeAdd = await run('get programs initial', getPrograms())
+    t.equal(programsBeforeAdd.length, 1, 'charlie has 1 program')
+
+    await run(
+      'adding program',
+      addProgram(entropy, { programPointer: programPointer1, programConfig: '' })
+    )
+    const programsAfterAdd = await run('get programs after add', getPrograms())
+    t.equal(programsAfterAdd.length, 2, 'charlie has 2 programs')
+
+    t.end()
   })
 
-  t.test('View Program', async vp => {
-    const runVp = promiseRunner(vp)
-    const programs = await runVp('get charlie programs', viewPrograms(entropy, { verifyingKey: entropy.programs.verifyingKey }))
+  t.test('programs - remove', async t => {
+    const run = promiseRunner(t)
 
-    vp.equal(programs.length, 1, 'charlie has 1 program')
-    vp.end()
+    const programsBeforeRemove = await run('get programs initial', getPrograms())
+    t.equal(programsBeforeRemove.length, 2, 'charlie has 2 programs')
+
+    await run(
+      'removing noop program',
+      removeProgram(entropy, { programPointer: programPointer1, verifyingKey })
+    )
+    const programsAfterRemove = await run('get programs initial', getPrograms())
+    t.equal(programsAfterRemove.length, 1, 'charlie has 1 less program')
+
+    t.end()
+  })
+
+  t.test('programs - view', async t => {
+    const run = promiseRunner(t)
+
+    const programs = await run(
+      'get charlie programs',
+      viewPrograms(entropy, { verifyingKey })
+    )
+
+    t.equal(programs.length, 1, 'charlie has 1 program')
+
+    t.end()
   })
 })
