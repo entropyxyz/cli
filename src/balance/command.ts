2261ee4bd17a33e5a6423e2a49608896c4b7e18b
mixmix <mix@protozoa.nz>
2024-08-28 13:10:40 +1200

    more tweeks

--- src/balance/command.ts
+++ src/balance/command.ts
@@ -4,13 +4,8 @@ import { cliWrite, endpointOption, passwordOption } from "src/common/utils-cli";
 import { EntropyBalance } from "./main";
 
 export async function entropyBalanceCommand (entropy: Entropy, rootCommand: Command) {
-  const balanceCommand = rootCommand.command('balance')
-    .description('Commands to retrieive account balances on the Entropy Network')
-  entropyAccountBalance(entropy, balanceCommand)  
-}
-
-async function entropyAccountBalance (entropy: Entropy, balanceCommand: Command) {
-  balanceCommand
+  rootCommand.command('balance')
+    .description('Command to retrieive the balance of an account on the Entropy Network')
     .argument('address', 'Account address whose balance you want to query')
     .addOption(passwordOption())
     .addOption(endpointOption())
@@ -20,4 +15,5 @@ async function entropyAccountBalance (entropy: Entropy, balanceCommand: Command)
       cliWrite(balance)
       process.exit(0)
     })
+
 }
--- src/balance/main.ts
+++ src/balance/main.ts
@@ -11,29 +11,24 @@ export class EntropyBalance extends EntropyBase {
 
   async getBalance (address: string): Promise<number> {
     const accountInfo = (await this.entropy.substrate.query.system.account(address)) as any
-    return parseInt(BalanceUtils.hexToBigInt(accountInfo.data.free).toString())
+    const balance = parseInt(BalanceUtils.hexToBigInt(accountInfo.data.free).toString())
+
+    this.logger.log(`Current balance of ${address}: ${balance}`, EntropyBalance.name)
+    return balance
   }
-  
+
   async getBalances (addresses: string[]): Promise<BalanceInfo> {
     const balanceInfo: BalanceInfo = {}
     await Promise.all(addresses.map(async address => {
       try {
         const balance = await this.getBalance(address)
-        
+
         balanceInfo[address] = { balance }
       } catch (error) {
         balanceInfo[address] = { error: error.message }
       }
     }))
-    
+
     return balanceInfo
-  }
-
-  async getAccountBalance (address: string) {
-    const balance = await this.getBalance(address)
-
-    this.logger.log(`Current balance of ${address}: ${balance}`, EntropyBalance.name)
-    
-    return `${balance.toLocaleString('en-US')} BITS`
   }
 }
