d998d4f53fab6c2bd1253a626f07aa1b9f02a63d
mixmix <mix@protozoa.nz>
2024-08-28 13:14:46 +1200

    fix tests

--- tests/sign.test.ts
+++ tests/sign.test.ts
@@ -1,9 +1,9 @@
 import test from 'tape'
 import { EntropySign } from '../src/sign/main'
 
-
 import { setupTest, charlieStashSeed } from './testing-utils'
 const endpoint = 'ws://127.0.0.1:9944'
+
 test('Sign - signWithAdapter', async (t) => {
   const { run, entropy } = await setupTest(t, { seed: charlieStashSeed })
   const SigningService = new EntropySign(entropy, endpoint)
@@ -11,7 +11,7 @@ test('Sign - signWithAdapter', async (t) => {
   await run('register', entropy.register())
   const signature = await run(
     'sign',
-    SigningService.signWithAdapters({ msg: "heyo!" })
+    SigningService.signMessageWithAdapters({ msg: "heyo!" })
   )
 
   t.true(signature && signature.length > 32, 'signature has some body!')
