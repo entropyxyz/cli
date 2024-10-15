185778bed7d044e624af85f54cd615f22f932b58
Nayyir Jutha <nayyir.jutha@gmail.com>
2024-10-15 12:27:09 -0400

    [NayNay] Faucet Loader
    - resolves #236
    
    - adds spinner to faucet, using new lightweight library yocto-spinner

--- src/faucet/interaction.ts
+++ src/faucet/interaction.ts
@@ -1,4 +1,5 @@
 import Entropy from "@entropyxyz/sdk"
+import yoctoSpinner from 'yocto-spinner';
 import { EntropyLogger } from '../common/logger'
 import { TESTNET_PROGRAM_HASH } from "./utils"
 import { EntropyFaucet } from "./main"
@@ -8,7 +9,11 @@ let chosenVerifyingKeys = []
 const amount = "10000000000"
 // context for logging file
 const FLOW_CONTEXT = 'ENTROPY_FAUCET_INTERACTION'
+const faucetSpinner = yoctoSpinner({text: 'Funding account…'})
 export async function entropyFaucet (entropy: Entropy, options, logger: EntropyLogger) {
+  if (faucetSpinner.isSpinning) {
+    faucetSpinner.stop()
+  }
   const { endpoint } = options
   if (!entropy.registrationManager.signer.pair) {
     throw new Error("Keys are undefined")
@@ -22,18 +27,29 @@ export async function entropyFaucet (entropy: Entropy, options, logger: EntropyL
 // Method that takes in the initial list of verifying keys (to avoid multiple calls to the rpc) and recursively retries each faucet until
 // a successful transfer is made
 async function sendMoneyFromRandomFaucet (entropy: Entropy, endpoint: string, verifyingKeys: string[], logger: EntropyLogger) {
+  if (!faucetSpinner.isSpinning) {
+    faucetSpinner.start()
+  }
   const faucetService = new EntropyFaucet(entropy, endpoint)
   const selectedAccountAddress = entropy.keyring.accounts.registration.address
-  const { chosenVerifyingKey, faucetAddress } = faucetService.getRandomFaucet(chosenVerifyingKeys, verifyingKeys)
+  let chosenVerifyingKey: string
   try {
+    const randomFaucet = faucetService.getRandomFaucet(chosenVerifyingKeys, verifyingKeys)
+    chosenVerifyingKey = randomFaucet.chosenVerifyingKey
+    const { faucetAddress } = randomFaucet
     await faucetService.sendMoney({ amount, addressToSendTo: selectedAccountAddress, faucetAddress, chosenVerifyingKey, faucetProgramPointer: TESTNET_PROGRAM_HASH })
     // reset chosen keys after successful transfer
+    if (faucetSpinner.isSpinning) faucetSpinner.stop()
     chosenVerifyingKeys = []
     print(`Account: ${selectedAccountAddress} has been successfully funded with ${parseInt(amount).toLocaleString('en-US')} BITS`)
   } catch (error) {
     logger.error('Error issuing funds through faucet', error, FLOW_CONTEXT)
     chosenVerifyingKeys.push(chosenVerifyingKey)
-    if (error.message.includes('FaucetError') || chosenVerifyingKeys.length === verifyingKeys.length) {
+    if (error.message.includes('FaucetError')) {
+      faucetSpinner.text = 'Faucet has failed...'
+      if (faucetSpinner.isSpinning) {
+        faucetSpinner.stop()
+      }
       console.error('ERR::', error.message)
       return
     } else {
