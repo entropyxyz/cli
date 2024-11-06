0fcd01a3689d18bfaf7f9694a19d147fbc7f43e0
Nayyir Jutha <nayyir.jutha@gmail.com>
2024-11-05 20:58:30 -0600

    [NayNay] Testnet Faucet
    - updated faucet constants to use new program mod key and program pointer deployed from boot script

--- src/faucet/utils.ts
+++ src/faucet/utils.ts
@@ -1,9 +1,9 @@
 // Testnet address used to deploy program on chain
 // Used to derive various accounts registered to faucet program in order to be used for
 // issuing Faucet Funds
-export const FAUCET_PROGRAM_MOD_KEY = '5GWamxgW4XWcwGsrUynqnFq2oNZPqNXQhMDfgNH9xNsg2Yj7'
+export const FAUCET_PROGRAM_MOD_KEY = '5EFD5zPcWzzjmXwJzfo4vKMiPcsAX74Y4kMmeEijCTJHcqij'
 // Faucet program pointer
 // To-DO: Look into deriving this program from owned programs of Faucet Program Mod Acct
 // this is differnt from tests because the fauce that is live now was lazily deployed without schemas
 // TO-DO: update this when faucet is deployed properly 
-export const TESTNET_PROGRAM_HASH = '0x12af0bd1f2d91f12e34aeb07ea622c315dbc3c2bdc1e25ff98c23f1e61106c77'
+export const TESTNET_PROGRAM_HASH = '0x3a1d45fecdee990925286ccce71f78693ff2bb27eae62adf8cfb7d3d61e142aa'
