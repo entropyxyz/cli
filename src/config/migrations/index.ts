582ddba49dddf881880e8ac8dbe817f1922faf56
mixmix <mix@protozoa.nz>
2024-11-07 09:49:45 +1300

    migration: remove existing vks - network state wipe

--- src/config/migrations/index.ts
+++ src/config/migrations/index.ts
@@ -2,12 +2,14 @@ import * as migration00 from './00'
 import * as migration01 from './01'
 import * as migration02 from './02'
 import * as migration03 from './03'
+import * as migration04 from './04'
 
 const migrations = [
   migration00,
   migration01,
   migration02,
-  migration03
+  migration03,
+  migration04
 ]
 
 export default migrations
