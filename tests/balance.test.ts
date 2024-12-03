c844dbbacc325e575d3392864a030b6c42aaf56f
mixmix <mix@protozoa.nz>
2024-12-03 16:52:23 +1300

    fix the other test

--- tests/balance.test.ts
+++ tests/balance.test.ts
@@ -41,8 +41,9 @@ test('getBalance + getBalances', async (t) => {
     balanceService.getBalances(badAddresses)
   )
 
-  badAddresses.forEach(addr => {
-    t.true(!!balancesWithNoGoodAddress[addr].error, `error field is populated for ${addr}`)
+  badAddresses.forEach((addr) => {
+    const match = balancesWithNoGoodAddress.find(info => info.address === addr)
+    t.true(!!match.error, `error field is populated for ${addr}`)
   })
 
   // TODO:
